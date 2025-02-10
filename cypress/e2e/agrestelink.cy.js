bd468ff190013e24961d40124c4b31605a97d6c7
Ademilton Fernandes <ademilton41@gmail.com>
2025-02-10 00:01:34 -0300

    Update agrestelink.cy.js

--- cypress/e2e/agrestelink.cy.js
+++ cypress/e2e/agrestelink.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://agrestelink.com.br')
+    cy.get('.elementor-element-b3aa229 > .elementor-element > .elementor-widget-container > .elementor-button-wrapper > .elementor-button').click()
   })
 })
