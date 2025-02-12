23d94ef8f715fe61c6a0d6bcc83abf02aa8d0e31
ademiltonf <ademilton41@gmail.com>
2025-02-12 11:23:28 -0300

    Update centraldocliente.cy.js

--- cypress/e2e/centraldocliente.cy.js
+++ cypress/e2e/centraldocliente.cy.js
@@ -16,14 +16,40 @@ describe('Central Agreste Link', () => {
 
   it('Login com sucesso e botão contatos', () => {
     cy.get('#cpfcnpj').should('be.visible').click(); // Aguarda o campo CPF/CNPJ aparecer
-    cy.get('#cpfcnpj').type("49840018442");
-    cy.get('form > .btn').should('be.visible').click();
+    cy.get('#cpfcnpj').type("49840018442"); // adicionei o cpf
+    cy.get('form > .btn').should('be.visible').click(); // clicou em acessar
     cy.get('.filter-option-inner-inner').should('be.visible').click();
     cy.get('#bs-select-1-1 > .text').should('be.visible').click();
     cy.get('.btn-primary').should('be.visible').click();
-    cy.get(':nth-child(19) > .nav-link').click()
-    
+    cy.get(':nth-child(19) > .nav-link').click();
   })
+
+    it('Login com sucesso + botão termo de contrato e fazer download',() => {
+      cy.get('#cpfcnpj').should('be.visible').click(); // Aguarda o campo CPF/CNPJ aparecer
+      cy.get('#cpfcnpj').type("49840018442"); // adicionei o cpf
+      cy.get('form > .btn').should('be.visible').click(); // clicou em acessar
+      cy.get('.filter-option-inner-inner').should('be.visible').click();
+      cy.get('#bs-select-1-1 > .text').should('be.visible').click();
+      cy.get('.btn-primary').should('be.visible').click();
+      cy.get(':nth-child(9) > .nav-link').should('be.visible').click();
+      cy.get('.modal-body > .btn').click()
+    })
+
+    it.only(" login com sucesso + acessar fatura", () => {
+      cy.get('#cpfcnpj').should('be.visible').click(); // Aguarda o campo CPF/CNPJ aparecer
+      cy.get('#cpfcnpj').type("49840018442"); // adicionei o cpf
+      cy.get('form > .btn').should('be.visible').click(); // clicou em acessar
+      cy.get('.filter-option-inner-inner').should('be.visible').click();
+      cy.get('#bs-select-1-1 > .text').should('be.visible').click();
+      cy.get('.btn-primary').should('be.visible').click();
+      cy.get(':nth-child(4) > .nav-link').should('be.visible').click()
+      //cy.get('.btn-primary').should('be.visible').click()
+      cy.get('.btn-info').scrollIntoView()
+      cy.get('.btn-info').click()
+
+    })
+    
+  
 
 })
 
